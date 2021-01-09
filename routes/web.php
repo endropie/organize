c13dae35efe5e9ecad840922c587d0c1eb03f372
endropie <hendrowibowo@msn.com>
2021-01-10 02:09:47 +0700

    FIX: Quasar & Brezee Auth

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () { return view('welcome'); });
+
+require __DIR__.'/auth.php';
+Route::get('/dashboard', function () { return view('dashboard'); })->middleware(['auth'])->name('dashboard');
+
+
+// Route::get('{any}', function () { return view('admin'); })->where('any','.*');
+Route::get('admin/{any}', function () { return view('admin'); })->where('any','.*');
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix
+    .js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+        require('postcss-import'),
+        require('tailwindcss'),
+        require('autoprefixer'),
+    ])
+    .copy('quasar/dist/spa/index.html', 'resources/views/admin.blade.php')
+    .copyDirectory('quasar/dist/spa', 'public/admin');
