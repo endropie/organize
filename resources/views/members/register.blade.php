db94b7c270479c14fd58a407bef87ab5a4ec418d
endropie <hendrowibowo@msn.com>
2021-03-01 21:28:19 +0700

    FIXED: Member register set meta description

--- resources/views/members/register.blade.php
+++ resources/views/members/register.blade.php
@@ -1,3 +1,6 @@
+@section('title', 'Pendaftaran Anggota IK2T - KP Tanjuang')
+@section('description', 'Pendaftaran anggota IK2T Kampuang tanjuang, IV koto Aur Malintang. kab Padang pariaman.')
+
 <x-website-layout>
   @livewire('member-register')
 </x-website-layout>
